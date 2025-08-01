9e6c3e14941d2beb7304e74a19d3253d57c992ff
Renato Alves <renatoft89@gmail.com>
2025-08-01 20:50:08 -0300

    fix: CREATE perguntas

--- src/services/perguntas.service.js
+++ src/services/perguntas.service.js
@@ -1,6 +1,9 @@
 const PerguntasModel = require('../models/Perguntas.Model');
 
 const cadastrarPergunta = async (pergunta) => {
+
+  console.log('Iniciando o cadastro de pergunta:', pergunta);
+  
   if (!pergunta.id_topico || !pergunta.descricao_pergunta || !pergunta.ordem_pergunta) {
     throw new Error('Dados obrigatórios faltando');
   }
