d2e87d6679574029ae2805b83096a616bc59bf0d
Renato <renatoft89@gmail.com>
2025-11-17 21:51:50 -0300

    Fix: configurações reset

--- README.md
+++
@@ -1,2 +0,0 @@
-# rto-auditorias
-Realizar, registrar e acompanhar auditorias técnicas operacionais (RTO) em empresas do ramo alimentício, garantindo conformidade com requisitos de segurança alimentar, higiene, manipulação e estrutura. 
--- src/routes/perguntas/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const perguntasController = require('../../controllers/perguntas.controller');
-const authMiddleware = require('../../middlewares/auth');
-
-router.put('/status/:id', authMiddleware, perguntasController.atualizarStatus);
-
-module.exports = router;
